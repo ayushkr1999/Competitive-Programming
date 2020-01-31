9a78891e3430068c1fb7992df9e11289b1b5652a
Ayush Kumar <59920636+ayushkr1999@users.noreply.github.com>
2020-01-31 18:35:48 +0530

    Create 4A.cpp

---
+++ CodeForces/4A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<set>
+#include<vector>
+#include<string>
+#define lli long long int
+#include<algorithm>
+using namespace std;
+
+int main() {
+	// your code goes here
+	lli a,t,b;
+	string c;
+	cin>>t;
+	if(t==2){
+		cout<<"NO";
+	}
+	else if(t%2==0){
+		cout<<"YES";
+	}
+	else{
+		cout<<"NO";
+	}
+	return 0;
+}
