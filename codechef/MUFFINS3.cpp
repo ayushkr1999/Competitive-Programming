e5d254f75752ade848e7462660bd14c8992e8a82
Ayush Kumar <59920636+ayushkr1999@users.noreply.github.com>
2020-02-02 00:46:25 +0530

    Create MUFFINS3.cpp

---
+++ codechef/MUFFINS3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<set>
+#include<vector>
+#define lli long long int
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+int main() {
+	// your code goes here
+	lli n,m,t,q,c,temp,ans;
+	
+	// c=0;
+	cin>>t;
+	while(t--){
+		cin>>n;
+		
+		cout<<(n/2)+1<<endl;
+	}
+	
+	return 0;
+}
