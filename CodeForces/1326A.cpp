6d59ddebe22efee4990f5079abcadfef441ce2f8
Ayush Kumar <59920636+ayushkr1999@users.noreply.github.com>
2020-03-29 21:27:19 +0530

    Update 1326A.cpp

--- CodeForces/1326A.cpp
+++ CodeForces/1326A.cpp
@@ -2,36 +2,32 @@
 #include <bits/stdc++.h>
 #define lli long long int
 using namespace std;
- 
+
 int main() {
 	// your code goes here
+	/*the basic idea is simple if 
+	n=1  there is  no solution
 	
-	lli t,n,m,o,e,a,maxi;
-	// cin>>t;
-	// while(t--){
-	cin>>n;
-	
-	vector<lli> v1(n);
-	for(lli i=0;i<n;i++)cin>>v1[i];
-	vector<lli> v2(n,0);
-	maxi=0;
-	for(lli i=0;i<n;i++){
-		
-		v2[i]=maxi+v1[i];
-		maxi=max(maxi,maxi+v1[i]);
+	else  answer is 23333.... where the length of this number is 'n' 
+	as 23 is not divisible by 2 and 3 so +3 for every digit is 
+	not divisible by 3 and not by 2 as well as the last digit is always 3
+	which is not divisble by 2.
+
+	*/
+	lli t,n,m,o,e,a;
+	cin>>t;
+	while(t--){
+		cin>>n;
+		if(n==1){
+			cout<<-1<<endl;
+		}
+		else{
+			cout<<2;
+			while(--n){
+				cout<<3;	
+			}
+			cout<<endl;
+		}
 	}
-	// for(lli i=0;i<n;i++){
-	// 	v1[i]=v1[i]-v2[i];
-	// }
-	// for(auto x:v1){
-	// 	cout<<x<<" ";
-	// }
-	// cout<<endl;
-	for(auto x:v2){
-		cout<<x<<" ";
-	}
-	// cout<<endl;
-		
-	// }
 	return 0;
 }
