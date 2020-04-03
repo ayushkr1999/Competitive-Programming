d49e6ad55fb958131fe0b5d3e3f43868b583586e
Ayush Kumar <59920636+ayushkr1999@users.noreply.github.com>
2020-04-03 20:03:08 +0530

    Create 1325A.cpp

---
+++ CodeForces/1325A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define lli long long int
+using namespace std;
+
+int main() {
+	// your code goes here
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+  
+	lli t,n,m,o,e,a,k;
+	cin>>t;
+	while(t--){
+		
+		cin>>n;
+		
+		lli ans=0;
+
+		cout<<1<<" "<<n-1<<endl;	
+	}
+	
+	return 0;
+}
