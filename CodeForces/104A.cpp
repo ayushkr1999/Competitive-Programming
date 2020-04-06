93a47148ddaf36d30c3d8e329e26f285e7529b1f
Ayush Kumar <59920636+ayushkr1999@users.noreply.github.com>
2020-04-06 17:17:15 +0530

    Create 104A.cpp

---
+++ CodeForces/104A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define lli long long int
+using namespace std;
+
+int main() {
+// your code goes here
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+
+lli t,n,m,o,s,e,a,k;
+lli x,y,z;
+cin>>n;
+// cin>>t;
+lli pl=0;
+lli c=0;
+// lli s3=0;
+// while(t--){
+	
+	if(n<=10 || n>21 )cout<<0;
+	if(n==20)cout<<15;
+	if(n==21 || (10<n && n<20))cout<<4;
+    
+
+// }
+// cout<<c;
+// cout<<(s1 ==0 && (s1==s2 && s1==s3)?"YES":"NO");
+return 0;
+}
