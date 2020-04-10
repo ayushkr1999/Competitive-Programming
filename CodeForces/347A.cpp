42256a5782e2260f8f989d27227fe2e1974bbf75
Ayush Kumar <59920636+ayushkr1999@users.noreply.github.com>
2020-04-10 17:57:26 +0530

    Create 347A.cpp

---
+++ CodeForces/347A.cpp
@@ -0,0 +1,73 @@
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+
+#define ll long long
+#define ld long double
+#define lli long long int
+#define pb push_back
+#define INF 1000000000
+#define mod 1000000007
+#define MOD 1000000007
+//#define mp make_pair
+#define loop(n) for (lli i = 0; i < n; i++)
+
+#define FOR(i,a,b) for (lli i = a; i < b; i+=1)
+
+
+#define st(v) v.begin(),v.end()
+
+
+#define F first
+#define S second
+
+#define itr :: iterator it
+#define w(t) cin>>t; while(t --)
+
+
+
+#define inputarr(a,n) for(lli i=0;i<n;i++)cin>>a[i];
+#define input(a,n) for(lli i=0;i<n;i++){ lli x ; cin>>x; a.push_back(x);}
+
+#define printarr(a,n) for(lli i=0;i<n;i++)cout<<a[i]; cout<<endl;
+#define print(vec) for(auto xt : vec)cout<<xt; cout<<endl;
+#define printset(st) for(auto xt : st) cout<<xt<<" ";    cout<<endl;
+
+
+int main() {
+// your code goes here
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ 
+	lli t,m,o,e,a,k,b;
+	lli x,y,z;
+	lli n;
+	lli f=0;
+	cin>>n;
+	lli maxi=2;
+	std::vector<lli> v1;
+	std::vector<lli> v2(n);
+	input(v1,n)
+	lli c=2;
+	// if(n==2)cout<<2;
+	// else{
+	loop(n)v2[i]=v1[i];
+
+	sort(st(v1));
+	sort(st(v2));
+	reverse(st(v2));
+	cout<<v2[0]<<" ";
+	for(lli i=1;i<n-1;i++){
+		cout<<v1[i]<<" ";
+	}
+	cout<<v1[0];
+
+
+	return 0;
+}
