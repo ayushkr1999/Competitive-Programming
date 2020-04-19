0eff05de8014aaf5a33e69c3e5dae92a62281ccf
Ayush Kumar <59920636+ayushkr1999@users.noreply.github.com>
2020-04-20 00:53:19 +0530

    Create 1180B.cpp

---
+++ CodeForces/1180B.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+
+#define ll long long
+#define ld long double
+#define lli long long int
+#define pb push_back
+#define INF 1000000000
+#define mod 1000000007
+#define MOD 1000000007
+//#define mp make_pair
+#define loop(n) for (lli i = 0; i < n; i++)
+#define FOR(i,a,b) for (lli i = a; i < b; i+=1)
+#define st(v) v.begin(),v.end()
+#define F first
+#define S second
+#define itr :: iterator it
+#define w(t) cin>>t; while(t --)
+#define inputarr(a,n) for(lli i=0;i<n;i++)cin>>a[i];
+#define input(a,n) for(lli i=0;i<n;i++){ lli x ; cin>>x; a.push_back(x);}
+#define printarr(a,n) for(lli i=0;i<n;i++)cout<<a[i]; cout<<endl;
+#define print(vec) for(auto xt : vec)cout<<xt; cout<<endl;
+#define printset(st) for(auto xt : st) cout<<xt<<" ";    cout<<endl;
+
+
+
+
+int main() {
+// your code goes here
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ 
+	// lli t,m,o,e,a,k,b,l,r;
+	lli x,y,z,k,a,b,c,d,m;
+	lli n,t;
+
+		cin>>n;
+		// cout<<n;
+		std::vector<lli> v1;
+		input(v1,n)
+		// lli p=1;
+		for(lli i =0;i<n;i++){
+			if(v1[i]>=0){
+				v1[i]=-1*v1[i]-1;
+			}
+			// p*=v1[i];
+
+		}
+
+		if(n%2==0){
+			for(auto x:v1){
+				cout<<x<<" ";
+			}
+		}
+		else{
+			lli mini=INF;
+			for(auto x: v1){
+				mini=min(mini,x);
+			}
+			for(lli i =0;i<n;i++){
+				if(v1[i]==mini){
+					v1[i]=-1*v1[i]-1;
+					break;
+				}
+			}
+			for(auto x: v1){
+				cout<<x<<" ";
+			}
+		}
+	
+
+
+	return 0;
+}
