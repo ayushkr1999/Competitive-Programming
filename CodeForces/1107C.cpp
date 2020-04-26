c7e4997754381c959e76823b7c116715c31c9335
Ayush Kumar <59920636+ayushkr1999@users.noreply.github.com>
2020-04-26 16:25:07 +0530

    Create 1107C.cpp

---
+++ CodeForces/1107C.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+#define N 100005
+#define ll long long
+#define ld long double
+#define lli long long int
+#define pb push_back
+#define INF 10000000000
+#define mod 1000000007
+#define MOD 1000000007
+//#define mp make_pair
+#define loop(n) for (lli i = 0; i < n; i++)
+#define FOR(i,a,b) for (lli i = a; i < b; i+=1)
+#define st(v) v.begin(),v.end()
+#define F first
+#define S second
+#define itr :: iterator it
+#define w(t) cin>>t; while(t --)
+#define inputarr(a,n) for(lli i=0;i<n;i++)cin>>a[i];
+#define input(a,n) for(lli i=0;i<n;i++){ lli x ; cin>>x; a.push_back(x);}
+#define printarr(a,n) for(lli i=0;i<n;i++)cout<<a[i]; cout<<endl;
+#define print(vec) for(auto xt : vec)cout<<xt; cout<<endl;
+#define printset(st) for(auto xt : st) cout<<xt<<" ";    cout<<endl;
+
+
+// lli v1[N], v2[N];
+lli gcd(lli a,lli b){
+ if(a==0){
+ 	return b;
+ }
+ gcd(b%a,a);
+}
+bool sign(lli a){
+	return a>0;
+}
+
+// int n, k, m,l,a[200004],i;
+int main() {
+// your code goes here
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ 
+	lli t,m,o,e,a,k,b,l,r,c,d;
+	// lli x,y,z,k,b,j,c,d,m;
+	lli n;
+
+
+		cin>>n>>k;
+		string s;
+		std::vector<lli> v2;
+		input(v2,n)
+		cin>>s;
+		lli i=0;
+		lli j=0;
+		while(j<n){
+			lli find=s[i];
+			while(s[j]==find){
+				j++;
+			}
+			sort(v2.begin()+i,v2.begin()+j);
+			reverse(v2.begin()+i,v2.begin()+j);
+			i=j;
+		}
+	
+		lli ans=0;
+		i=0;
+		j=0;
+		while(j<n){
+			lli find=s[i];
+			lli c=k;
+			while(s[j]==find ){
+				if(c>0)
+					ans+=v2[j];
+				j++;
+				c--;
+			}
+			i=j;
+
+
+		}
+		cout<<ans;
+		
+	
+
+	return 0;
+}
