88a2ba1eb80c061ce8675c1157087661cb97b083
Ayush Kumar <59920636+ayushkr1999@users.noreply.github.com>
2020-04-27 17:13:01 +0530

    Create 1150A.cpp

---
+++ CodeForces/1150A.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+#define N 1000005
+#define ll long long
+#define ld long double
+#define lli long long int
+#define pb push_back
+#define INF 10000000000
+#define mod 1000000007
+#define MOD 1000000007
+//#define mp make_pair
+#define loop(n) for (lli i = 0; i < n; i++)
+#define FOR(i,a,b) for (lli i = a; i < b; i+=1)
+#define st(v) v.begin(),v.end()
+#define F first
+#define S second
+#define itr :: iterator it
+#define w(t) cin>>t; while(t --)
+#define inputarr(a,n) for(lli i=0;i<n;i++)cin>>a[i];
+#define input(a,n) for(lli i=0;i<n;i++){ lli x ; cin>>x; a.push_back(x);}
+#define printarr(a,n) for(lli i=0;i<n;i++)cout<<a[i]; cout<<endl;
+#define print(vec) for(auto xt : vec)cout<<xt; cout<<endl;
+#define printset(st) for(auto xt : st) cout<<xt<<" ";    cout<<endl;
+
+
+lli v1[N], v2[N];
+// lli ans[N], a[N];
+lli gcd(lli a,lli b){
+ if(a==0){
+ 	return b;
+ }
+ gcd(b%a,a);	
+}
+bool sign(lli a){
+	return a>0;
+}
+
+// int n, k, m,l,a[200004],i;
+int main() {
+// your code goes here
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ 
+	lli t,m,o,e,a,k,b,l,r,c,d;
+	// lli x,y,z,k,b,j,c,d,m;
+	lli n;
+	
+
+		cin>>n>>m>>k;
+		
+		inputarr(v1,n)
+		inputarr(v2,m)
+		sort(v1,v1+n);
+		lli maxi= *max_element(v2,v2+m);
+		lli mini= *min_element(v1,v1+n);
+		
+		if(mini>=maxi){
+			cout<<k;
+		}	
+		else{
+			cout<<k+k/mini*(maxi-mini);
+		}
+
+
+
+		
+		
+
+
+
+
+	return 0;
+}
