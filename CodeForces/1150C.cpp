6a9243c41191675d86ada70b345db4fd9e92f144
Ayush Kumar <59920636+ayushkr1999@users.noreply.github.com>
2020-05-07 22:13:45 +0530

    Update 1150C.cpp

--- CodeForces/1150C.cpp
+++ CodeForces/1150C.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define fastIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
-#define N 1000005
+#define N 100005
 #define ll long long
 #define ld long double
 #define lli long long int
@@ -23,24 +23,14 @@ using namespace std;
 #define printarr(a,n) for(lli i=0;i<n;i++)cout<<a[i]; cout<<endl;
 #define print(vec) for(auto xt : vec)cout<<xt; cout<<endl;
 #define printset(st) for(auto xt : st) cout<<xt<<" ";    cout<<endl;
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp> 
-using namespace __gnu_pbds; 
-// typedef tree<int,null_type,less<int>,rb_tree_tag,
-// tree_order_statistics_node_update> indexed_set;
-typedef tree<int, null_type, less_equal<int>, 
-             rb_tree_tag, 
-             tree_order_statistics_node_update> 
-    ordered_set; 
 
 
-lli v1[N], v2[N];
-// lli ans[N], a[N];
+// lli v1[N], v2[N];
 lli gcd(lli a,lli b){
  if(a==0){
  	return b;
  }
- gcd(b%a,a);	
+ gcd(b%a,a);
 }
 bool sign(lli a){
 	return a>0;
@@ -54,54 +44,58 @@ freopen("input.txt","r",stdin);
 freopen("output.txt","w",stdout);
 #endif
  
-	lli t,m,o,e,a,k,b,l,r,c,d,x,y,f;
+	lli t,m,o,e,a,k,b,l,r,c,d;
 	// lli x,y,z,k,b,j,c,d,m;
-	
-	// w(t){
 	lli n;
-	// lli v1[3];
-	cin>>n;
-	char v1[n][n];
-	// bool v2[n][n];
+	// lli f=0;
+		
+		
+	// w(t){
 
-	for(lli i =0;i<n;i++){
-		for(lli j=0;j<n;j++){
-			cin>>v1[i][j];
-			// v2[i][j]=1;
-			
-
+		cin>>n;
+		string s;
+		map<lli,lli> has;
+		for(lli i =0;i<n;i++){
+			cin>>a;
+			has[a]++;
 		}
-	}
-  
-  // Don't intitialize i (from 1 to n-2 )and j (from 1 to n-2) 
-	for(lli i=0;i<n;i++){
-		for(lli j=0;j<n;j++){
-			if(v1[i][j]=='.'){
-				if(v1[i+1][j] =='.' && v1[i+2][j] =='.' && v1[i+1][j-1] =='.' && v1[i+1][j+1] =='.' ){
-					v1[i+1][j] = '#';
-					v1[i+2][j] = '#';
-					v1[i+1][j+1] = '#';
-					v1[i+1][j-1] = '#';
-					v1[i][j] = '#';
-				}
-				else{ 
-					cout<<"NO";
-					return 0;
+		if(has.size()==1){
+			for(auto x:has){
+				for(lli i =0;i<x.S;i++){
+					cout<<x.F<<" ";
 				}
 			}
 		}
-	}
-	
-	cout<<"YES";
-	
+		else{
+			cout<<2<<" "<<1<<" ";
+			has[2]--;
+			has[1]--;
+			for(lli i=0;i<has[2];i++){
+				cout<<2<<" ";
+			}
+			for(lli i=0;i<has[1];i++){
+				cout<<1<<" ";
+			}
+		}
+		// lli ans=0;
+		// lli maxi=-1*INF;
+		
+		// }
+		// cout<<ans;
+
+		
+		
+		
+
+		
 
 
 
-	
 
-	// cout<<endl;
+	// 	cout<<endl;
+		
 
 	// }
+
 	return 0;
 }
-
