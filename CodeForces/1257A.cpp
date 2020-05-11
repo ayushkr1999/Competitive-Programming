176abae2469adc4da1c82bd52910fb5070f18217
Ayush Kumar <59920636+ayushkr1999@users.noreply.github.com>
2020-05-11 17:36:14 +0530

    Create 1257A.cpp

---
+++ CodeForces/1257A.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+ 
+#define N 1000005
+#define ll long long
+#define ld long double
+#define lli long long int
+#define pb push_back
+#define INF 10000000000
+#define mod 1000000007
+#define MOD 1000000007
+#define mp make_pair
+#define loop(n) for (lli i = 0; i < n; i++)
+#define FOR(i,a,b) for (lli i = a; i < b; i+=1)
+#define st(v) v.begin(),v.end()
+#define F first
+#define S second
+#define mp make_pair
+#define itr :: iterator it
+#define w(t) cin>>t; while(t --)
+#define inputarr(a,n) for(lli i=0;i<n;i++)cin>>a[i];
+#define input(a,n) for(lli i=0;i<n;i++){ lli x ; cin>>x; a.push_back(x);}
+#define printarr(a,n) for(lli i=0;i<n;i++)cout<<a[i]; cout<<endl;
+#define print(vec) for(auto xt : vec)cout<<xt; cout<<endl;
+#define printset(st) for(auto xt : st) cout<<xt<<" ";    cout<<endl;
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+// typedef tree<int,null_type,less<int>,rb_tree_tag,
+// tree_order_statistics_node_update> indexed_set;
+typedef tree<int, null_type, less_equal<int>, 
+             rb_tree_tag, 
+             tree_order_statistics_node_update> 
+    ordered_set; 
+ 
+ 
+lli v1[N], v2[N];
+// lli ans[N], a[N];
+
+	
+
+ 
+// int n, k, m,l,a[200004],i;
+int main() {
+// your code goes here
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ 
+	lli m,o,e,a,k,b,l,c,d,x,y,f,t,r;
+	// lli x,y,z,k,b,j,c,d,m;
+	
+	w(t){
+
+	lli n;
+	// lli c=0;
+	cin>>n>>m>>a>>b;
+	if(a<b){
+		swap(a,b);
+	}
+
+	lli temp=n-a;
+	a+=min(temp,m);
+	m-=min(temp,m);
+	temp=b-1;
+	b-=min(m,temp);
+
+	cout<<a-b;
+	cout<<endl;
+
+ 
+	}
+	return 0;
+}
