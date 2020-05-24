0eeb6cd641d2da3a4c6c784943aaafa1a2306c54
Ayush Kumar <59920636+ayushkr1999@users.noreply.github.com>
2020-05-25 00:46:02 +0530

    Create 493Dternary.cpp

---
+++ CodeForces/493Dternary.cpp
@@ -0,0 +1,107 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define endl '\n'
+#define N 1000005
+#define ll long long
+#define ld long double
+#define lli long long	
+#define pb push_back
+#define INF 10000000000
+#define mod 1000000007
+#define MOD 1000000007
+#define mp make_pair
+#define loop(n) for (lli i = 0; i < n; i++)
+#define FOR(a,b) for (lli i = a; i < b; i+=1)
+#define st(v) v.begin(),v.end()
+#define F first
+#define S second
+#define mp make_pair
+#define itr :: iterator it
+#define w(t) cin>>t; while(t --)
+#define inputarr(a,n) for(lli i=0;i<n;i++)cin>>a[i];
+#define input(a,n) for(lli i=0;i<n;i++){ lli x ; cin>>x; a.push_back(x);}
+#define printarr(a,n) for(lli i=0;i<n;i++)cout<<a[i]; cout<<endl;
+#define print(vec) for(auto xt : vec)cout<<xt; cout<<endl;
+#define printset(st) for(auto xt : st) cout<<xt<<" ";    cout<<endl;
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+// typedef tree<int,null_type,less<int>,rb_tree_tag,
+// tree_order_statistics_node_update> indexed_set;
+typedef tree<int, null_type, less_equal<int>, 
+             rb_tree_tag, 
+             tree_order_statistics_node_update> 
+    ordered_set; 
+ 
+ 
+lli v1[N], v2[N], v3[N];
+
+lli A,R,M,n;
+lli a,b,x,y,m;
+
+lli f(lli k) {
+    lli ans = 0;
+    for (int i = 1; i <=n; i++) {
+        if ( v1[i] < k) {
+            ans += (k - v1[i]);
+        }
+    }
+    for (int i = 1; i <= m; i++) {
+        if (v2[i] > k) {
+            ans += (v2[i] - k);
+        }
+    }
+    return ans;
+}
+
+lli ternary(lli r){
+
+    lli l = 0;
+    for(lli i =0;i<100;i++){
+        lli ml = ( l + (r - l) / 3), mr = (r - (r - l) / 3);
+        //printf("%d %d %d %d\n",l,r,ml,mr);
+        
+        // cout<<l<<" "<<ml<<" "<<mr<<" "<<r<<endl;
+        if (f(mr)<f(ml))
+            l = ml+1;
+        else r = mr-1;
+    }
+    return f(l);
+}
+
+int main() {
+// your code goes here
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+ 
+	
+	fastIO
+	lli t,k,l,r;
+	
+  lli ans=0;
+  cin>>n>>m;
+  for (int i = 1; i <= n; i++)
+    cin >> v1[i];
+  for (int i = 1; i <= m; i++)
+      cin >> v2[i];
+
+
+  cout<<ternary(INF);
+	
+
+
+
+
+		
+		
+
+
+	
+	// }
+
+	return 0;
+
+}
